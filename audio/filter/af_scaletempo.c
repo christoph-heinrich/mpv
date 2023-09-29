cb0308b728988aede2287fb23e9b6fdb5cdf065c
Christoph Heinrich <christoph.heinrich@student.tugraz.at>
2023-09-29 20:30:41 +0200

    scaletempo: get rid of pre correlation buffer
    Without this optimization it's more obvious what's going on
    and better for testing, can be readded later.

--- audio/filter/af_scaletempo.c
+++ audio/filter/af_scaletempo.c
@@ -143,20 +143,15 @@ static int best_overlap_offset_float(struct priv *s)
     float best_corr = INT_MIN;
     int best_off = 0;
 
-    float *pw  = s->table_window;
-    float *po  = s->buf_overlap;
-    po += s->num_channels;
-    float *ppc = s->buf_pre_corr;
-    for (int i = s->num_channels; i < s->samples_overlap; i++)
-        *ppc++ = *pw++ **po++;
-
     float *search_start = (float *)s->buf_queue + s->num_channels;
     for (int off = 0; off < s->frames_search; off++) {
         float corr = 0;
         float *ps = search_start;
-        ppc = s->buf_pre_corr;
+        float *po = s->buf_overlap;
+        po += s->num_channels;
+        float *pw  = s->table_window;
         for (int i = s->num_channels; i < s->samples_overlap; i++)
-            corr += *ppc++ **ps++;
+            corr += *po++ * *ps++ * *pw++;
         if (corr > best_corr) {
             best_corr = corr;
             best_off  = off;
@@ -467,10 +462,9 @@ static bool reinit(struct mp_filter *f)
             }
             s->best_overlap_offset = best_overlap_offset_s16;
         } else {
-            s->buf_pre_corr = realloc(s->buf_pre_corr, s->bytes_overlap);
             s->table_window = realloc(s->table_window,
                                         s->bytes_overlap - nch * bps);
-            if (!s->buf_pre_corr || !s->table_window) {
+            if (!s->table_window) {
                 MP_FATAL(f, "Out of memory\n");
                 return false;
             }
